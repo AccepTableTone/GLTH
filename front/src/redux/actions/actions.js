8d75553e843f3bc55b82514b9e1aa63a08987bf0
david <david@davidmckinnon.ca>
2017-12-22 13:26:48 -0500

    - flight search: same origin/destination check

--- front/src/redux/actions/actions.js
+++ front/src/redux/actions/actions.js
@@ -2,21 +2,25 @@ import * as CONSTS from '../../config/constants';
 
 export function getFlights(origin, destination){
     return (dispatch) => {
-        dispatch(isLoading(true));
-        fetch(`${CONSTS.apiBaseUrl}/flights/${origin.IATA}/${destination.IATA}`)
-        .then((response) => {
-            if (!response.ok) {
+        if(origin.IATA === destination.IATA)
+            dispatch(haveFlightResults([], [], [], origin, destination));
+        else{
+            dispatch(isLoading(true));
+            fetch(`${CONSTS.apiBaseUrl}/flights/${origin.IATA}/${destination.IATA}`)
+            .then((response) => {
+                if (!response.ok) {
+                    dispatch(hasError("Sorry, there was a problem loading this page. Please refresh, and try again."));
+                }
+                return response;
+            })
+            .then(response => response.json())
+            .then(response =>  {
+                dispatch(haveFlightResults(response.Flights, response.Airports, response.Airlines, origin, destination));
+            })
+            .catch(error => {
                 dispatch(hasError("Sorry, there was a problem loading this page. Please refresh, and try again."));
-            }
-            return response;
-        })
-        .then(response => response.json())
-        .then(response =>  {
-            dispatch(haveFlightResults(response.Flights, response.Airports, response.Airlines, origin, destination));
-        })
-        .catch(error => {
-            dispatch(hasError("Sorry, there was a problem loading this page. Please refresh, and try again."));
-        });
+            });    
+        }
     };
 }
 
