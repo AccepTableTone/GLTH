831cdcdae6b9bdc9dd060f05b3970787a1bd3d52
david <david@davidmckinnon.ca>
2017-12-27 17:42:36 -0500

    - front: switched api url

--- front/src/config/constants.js
+++ front/src/config/constants.js
@@ -1,5 +1,5 @@
-//export const apiBaseUrl = "http://glth.acceptabletone.com/api";
-export const apiBaseUrl = "http://local-tmpapi/api";
+export const apiBaseUrl = "http://glth.acceptabletone.com/api";
+//export const apiBaseUrl = "http://local-tmpapi/api";
 
 export const ACTIONTYPE_ISLOADING = 'isloading';
 export const ACTIONTYPE_HASERROR = 'haserror';
